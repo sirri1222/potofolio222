0d380b3a9d9bc52a8d731e082381f06e32525d7b
sirri1222 <hyeonji159753@gmail.com>
2023-04-18 23:30:12 +0900

    푸터 수정

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div>
-      <p className=" absolute xl:left-[43%] xl:bottom-[10%] text-black sm:left-[39%] lg:bottom-[8%] lg:left-[40%] sm:bottom-[15%] xs:left-[20%]  ">
+      <p className=" absolute left-[50%] bottom-[10%] translate-x-[-50%]  ">
         ⓒ 2023.Sonhyunji all rights reserved.
       </p>
     </div>
